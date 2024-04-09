440b435b7608fdb29456c9fdf59b2859c4e6b493
thooton <83443912+thooton@users.noreply.github.com>
2024-04-09 18:31:43 +0000

    Delete index.py

--- index.py
+++
@@ -1,6 +0,0 @@
-# Print out the full pathnames of all files in the src/ directory
-import os
-
-for root, dirs, files in os.walk("src/"):
-    for file in files:
-        print(os.path.join(root, file)[4:])
